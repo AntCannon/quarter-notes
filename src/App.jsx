bdcf25c90312b48367f67fd7aa7d41540d692838
AntCannon <anthonycannonier@gmail.com>
2024-05-04 11:59:14 -0400

    add search static

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Nav from './components/Nav.jsx'
+import Search from './components/Search.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Nav />
+      <Search />
       
     </>
   )
