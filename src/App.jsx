39d03fccd41c0887ec047289ebe7a5b2b4a01f68
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-04 12:38:17 -0400

    cleaned up app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import "./components/NewNote.css"
 import Nav from './components/Nav.jsx'
 import Search from './components/Search.jsx'
-import NewNote from './components/new-note.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +11,6 @@ function App() {
     <>
       <Nav />
       <Search />
-      <NewNote />
     </>
   )
 }
