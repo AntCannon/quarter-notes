3e446cb9852f14664066f230f1de3f969ccaa2c3
AntCannon <anthonycannonier@gmail.com>
2024-05-04 11:36:36 -0400

    add nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+// Nav.jsx
+export default function Notes() {
+  
+  return (
+    <nav>
+      <span>New Note</span>
+      <span>About Us</span>
+      <span>Home</span>
+      <span>Menu</span>
+    </nav>
+  )
+}
