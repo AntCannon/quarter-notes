752c22a28cf62cf40b01d1b41b920d603d5557ed
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-04 12:24:27 -0400

    static and styling done

--- src/components/new-note.jsx
+++ src/components/new-note.jsx
@@ -5,36 +5,44 @@ export default function NewNote() {
     <div className="new-note_container">
       <h1 className="new-note-heading">New Note</h1>
       <form className="new-note_form">
-        <label className="new-note_form_label" htmlFor="title">
-          <br />
-          Title
-          <br />
-          <input
-            className="new-note_input"
-            type="text"
-            id="title"
-            name="title"
-          />
-        </label>
-        <label className="new-note_label" htmlFor="body">
-          <br />
-          Body
-          <br />
-          <textarea className="new-note_input" id="body" name="body"></textarea>
-        </label>
-        <label className="new-note_label" htmlFor="category">
-          <br />
-          Category
-          <br />
-          <select className="new-note_input" id="category">
-            <option>Please choose one option</option>
-            {options.map((option, i) => {
-              return <option key={i}>{option}</option>;
-            })}
-          </select>
-        </label>
-        <div className="new-note-preview">Note Preview</div>
-        <input className="new-note_submit" type="submit" value="submit" />
+        <div className="column-one">
+          <label className="new-note_form_label" htmlFor="title">
+            <br />
+            Title
+            <br />
+            <input
+              className="new-note_input"
+              type="text"
+              id="title"
+              name="title"
+            />
+          </label>
+          <label className="new-note_label" htmlFor="body">
+            <br />
+            Body
+            <br />
+            <textarea
+              className="new-note_input"
+              id="body"
+              name="body"
+            ></textarea>
+          </label>
+          <label className="new-note_label" htmlFor="category">
+            <br />
+            Category
+            <br />
+            <select className="new-note_input" id="category">
+              <option>Please choose one option</option>
+              {options.map((option, i) => {
+                return <option key={i}>{option}</option>;
+              })}
+            </select>
+          </label>
+        </div>
+        <div className="column-two">
+          <div className="new-note-preview">Note Preview</div>
+          <input className="new-note_submit" type="submit" value="Submit" />
+        </div>
       </form>
     </div>
   );
