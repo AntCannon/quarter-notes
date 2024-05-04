983a824faa3a275d405e12e2867b8e997a9aa135
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-05-04 15:44:28 -0400

    routes completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import "./App.css";
-import './components/Note.css'
+import "./components/Note.css";
 import "./components/NewNote.css";
 import Nav from "./components/Nav.jsx";
 import Search from "./components/Search.jsx";
-import Note from './components/Note.jsx'
-import Notes from './components/Notes.jsx'
-import NewNote from "./components/NewNote.jsx"
+import Note from "./components/Note.jsx";
+import Notes from "./components/Notes.jsx";
+import NewNote from "./components/NewNote.jsx";
+import About from "./components/About.jsx"
+import { Routes, Route } from "react-router-dom"; // npm i react-router-dom
 
 function App() {
   return (
     <>
       <Nav />
       <Search />
-      <Notes />
-      <Note />
-      <NewNote />
+      <Routes>
+        <Route path="/" element={<Notes />}/>
+        <Route path="/note/:id" element={<Note />}/>
+        <Route path="/new-note" element={<NewNote />}/>
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,13 @@
-// Nav.jsx
+import { Link } from "react-router-dom"
+
 export default function Notes() {
   
   return (
     <nav>
-      <span>New Note</span>
-      <span>About Us</span>
-      <span>Home</span>
-      <span>Menu</span>
+      <Link to="new-note"><span>New Note</span></Link>
+      <Link to=""><span>About Us</span></Link>
+      <Link to="/"><span>Home</span></Link>
+      <Link to="about"><span>Menu</span></Link>
     </nav>
   )
 }
