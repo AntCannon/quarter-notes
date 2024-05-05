ecec048c018bb025683611809aa38f3ec160ed1e
efierr <erickfierro0413@gmail.com>
2024-05-05 14:56:12 -0400

    Filter functionality complete

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,43 +1,15 @@
-import { useState, useEffect } from "react";
-import db from "../data/db.json"
-import "./Filters.css"
-import { getNotes } from "../utils/fetch.js"
+import React from "react";
+import "./Filters.css";
 
-export default function Filter() {
-  const [SearchText, SetSearchText] = useState("");
-  
-  useEffect(() =>{
-    getNotes()
-    .then(data => {
-      setNotes(data)
-    })
-    .catch(err => {
-      console.log(err)
-      console.log("local data")
-    })
-  }, [])
-
-  function handleTextChange(event) {
-    const title = event.target.value;
-    const result = title.length ? filterNotes(title, allNotes) : allNotes
-    SetSearchText(title);
-    setAllNotes(result);
-  }
-
-  function filterNotes(search, allNotes) {
-    return allNotes.filter((note) => {
-      return allNotes.title.match(title);
-    });
-  }
-
-    return (
+export default function Filter({ handleFilterChange }) {
+  return (
     <div className="filters">
-      <input 
-      id="search" 
-      type="text" 
-      placeholder="Search Notes"
-      value={SearchText}
-      onChange={handleTextChange} />
+      <input
+        id="search"
+        type="text"
+        placeholder="Search Notes"
+        onChange={(e) => handleFilterChange(e.target.value)}
+      />
     </div>
-  )
+  );
 }
