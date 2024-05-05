a4d40664bca937dea2dac6e10da1bb9a15c2b32f
efierr <erickfierro0413@gmail.com>
2024-05-05 15:10:12 -0400

    post-merge commit

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from "react"
-import { getNotes } from "../utils/fetch.js"
-import "./Notes.css"
-import NoteListing from "./NoteListing.jsx"
-
-console.log(db);
+import { useEffect, useState } from "react";
+import { getNotes } from "../utils/fetch.js";
+import "./Notes.css";
+import NoteListing from "./NoteListing.jsx";
 
 export default function Notes() {
-  const [ notes, setNotes ] = useState([]);
-  const [ isLoading, setIsLoading ] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
     getNotes()
-    .then(data => {
-      setNotes(data)
-      setIsLoading(false)
-    })
-    .catch(err => {
-      console.log(err)
-      setIsLoading(false)
-    })
-  }, [])
+      .then((data) => {
+        setNotes(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
 
   const filteredNotes = notes.filter((note) =>
     note.title.toLowerCase().includes(search.toLowerCase())
