a640abbd4037c44db880b45a88090c529720b903
AntCannon <anthonycannonier@gmail.com>
2024-05-06 15:35:20 -0400

    add link to repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function() {
     <footer>
       <p className="copyright">Quartech, LMTD ©2024</p>
       <p className="center">Footer</p>
-      <p className="source-code">See Source Code</p>
+      <a href="https://www.github.com/AntCannon/quarter-notes" target="blank"><p className="source-code">See Source Code</p></a>
     </footer>
   )
 }
