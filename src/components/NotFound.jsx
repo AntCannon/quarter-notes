f0dcd52dc44fbb5d04f650c72409f4396f24ed5b
AntCannon <anthonycannonier@gmail.com>
2024-05-06 08:42:43 -0400

    add music rest note and .css

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,13 @@
+import "./NotFound.css"
+import quarterRestNote from "../assets/quarter-rest-note.png"
 export default function NotFound() {
-    return (
-        <h1 style={{ color: 'red', textAlign: 'center' }}>Error 404: Page Not Found</h1>
-    )
+  return (
+    <div>
+
+      <h2>
+        Error 404: Page Not Found
+      </h2>
+      <img src={quarterRestNote} alt="quarter rest note error" />
+    </div>
+  )  
 }
